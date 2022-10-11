41b2054357183151ddc6f1829404f8598ebb39a8
mku777 <mkm.kuz777@gmail.com>
2022-10-11 17:47:24 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,6 @@
-const refs = {
-startButton: document.querySelector("button[data-start]"),
-stopButton: document.querySelector("button[data-stop]"),}
+
+const startButton = document.querySelector("button[data-start]");
+const stopButton = document.querySelector("button[data-stop]");
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
