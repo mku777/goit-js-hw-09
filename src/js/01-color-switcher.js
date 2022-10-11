f2fac8f51ca84b37ab78dd8b6410e495c09155ed
mku777 <mkm.kuz777@gmail.com>
2022-10-11 17:25:39 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,10 @@
-const startButton = document.querySelector("button[data-start]");
-const stopButton = document.querySelector("button[data-stop]");
-
-startButton.addEventListener('click', )
+const refs = {
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]'),
+};
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  };
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-  class ColorSwitcher {
-    constructor (swithchBgColor) {
-        this.isActive = false;
-        this.intervalID = null;
-        this.swithchBgColor = swithchBgColor;
-    }
-
-   
-
-    onStartChangeBgColor() {
-        if (this.isActive) {
-            return;
-        }
-    }
-  }
+startButton.addEventListener('click');
