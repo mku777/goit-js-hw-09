59e7fd682615442426a92ae111576462080ce9cc
mku777 <109515909+mku777@users.noreply.github.com>
2022-10-11 14:47:46 +0000

    Deploying to gh-pages from @ mku777/goit-js-hw-09@41b2054357183151ddc6f1829404f8598ebb39a8 🚀

--- 01-color-switcher.205f6c55.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button[data-start]"),document.querySelector("button[data-stop]");startButton.addEventListener("click");
-//# sourceMappingURL=01-color-switcher.205f6c55.js.map
