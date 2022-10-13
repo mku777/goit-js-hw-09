523d03c87c13ff141aabb4e03549cbbcfbb5adca
mku777 <109515909+mku777@users.noreply.github.com>
2022-10-13 14:34:52 +0000

    Deploying to gh-pages from @ mku777/goit-js-hw-09@4be66b5e243fd8fc3e4f0d45fc2e3a0590e95349 🚀

--- 03-promises.d57d2a1b.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(o){o.preventDefault()})),(void Math.random()).then((function(o){var n=o.position,e=o.delay;console.log("✅ Fulfilled promise ".concat(n," in ").concat(e,"ms"))})).catch((function(o){var n=o.position,e=o.delay;console.log("❌ Rejected promise ".concat(n," in ").concat(e,"ms"))}));
-//# sourceMappingURL=03-promises.d57d2a1b.js.map
