affb05e4ecc2c88dc6ad14878e83222de2803397
mku777 <mkm.kuz777@gmail.com>
2022-10-13 16:41:32 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,15 @@
+
+
+const formEl = document.querySelector(".form");
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit (event) {
+  event.preventDefault();
+
+}
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +18,12 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
