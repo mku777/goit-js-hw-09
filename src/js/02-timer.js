ad1310790801c93e4dc2fd6e19350b9bd136f202
mku777 <mkm.kuz777@gmail.com>
2022-10-14 11:38:15 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,14 +2,14 @@ import flatpickr from 'flatpickr';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'flatpickr/dist/flatpickr.min.css';
 
-
+let selectedTime = null;
 const inputDate = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('button[data-start]');
 const days = document.querySelector('span[data-days]');
 const hours = document.querySelector('span[data-hours]');
 const minutes = document.querySelector('span[data-minutes]');
 const seconds = document.querySelector('span[data-seconds]');
-let selectedTime = null;
+
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -30,14 +30,23 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-
+function addZero (value) {
+  return String(value).padStart(2, "0");
+}
 
 const options = {
   enableTime: true,
   time_24hr: true,
-  defaultDate: new Date(),
+  defaultDate: Date.now(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] < Date.now()) {
+      Notify.failure('Please choose a date in the future');
+      selectedDates[0] = new Date();
+    } else {
+      startButton.disabled = false;
+      selectedTime = selectedDates[0];
+    }
   },
 };
+
