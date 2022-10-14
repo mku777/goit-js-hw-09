45b2c95bc394c1eb06515443701e38ee20a4b6eb
mku777 <109515909+mku777@users.noreply.github.com>
2022-10-14 07:35:50 +0000

    Deploying to gh-pages from @ mku777/goit-js-hw-09@71065e022b79db9a0075782fc60ef510fc7aafab 🚀

--- 02-timer.747afe3f.js
+++
@@ -1,2 +0,0 @@
-!function(){new Date;function o(o){var e=6e4,n=36e5,l=24*n;return{days:Math.floor(o/l),hours:Math.floor(o%l/n),minutes:Math.floor(o%l%n/e),seconds:Math.floor(o%l%n%e/1e3)}}console.log(o(2e3)),console.log(o(14e4)),console.log(o(2414e4))}();
-//# sourceMappingURL=02-timer.747afe3f.js.map
